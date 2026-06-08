f33e7b0fe68e424acadbef309149fe6055c98e7e
pinkeshbhuwal-lgtm <pinkeshbhuwal@gmail.com>
2026-06-08 10:41:44 +0530

    Update workflow.tf

--- workflow.tf
+++ workflow.tf
@@ -2,35 +2,26 @@ resource "aws_glue_workflow" "workflow" {
   name = "etl-workflow"
 }
 
-resource "aws_glue_trigger" "start_job1" {
-
-  name = "start-job1"
-
+resource "aws_glue_trigger" "start_job1"{
+  name ="start-job1"
   type = "ON_DEMAND"
-
   workflow_name = aws_glue_workflow.workflow.name
 
-  actions {
-    job_name = aws_glue_job.job1.name
+  action {
+  job_name = aws_glue_job.job1.name
   }
 }
 
 resource "aws_glue_trigger" "job2_after_job1" {
+name = "job2-after-job1"
+type = "CONDITIONAL"
+workflow_name =aws_glue_workflow.workflow.name
 
-  name = "job2-after-job1"
-
-  type = "CONDITIONAL"
-
-  workflow_name = aws_glue_workflow.workflow.name
-
-  predicate {
-    conditions {
-      job_name = aws_glue_job.job1.name
-      state    = "SUCCEEDED"
-    }
-  }
-
-  actions {
-    job_name = aws_glue_job.job2.name
+predicate{
+  condition{
+    job_name = aws_glue_job.job1.name
+    state = "SUCCEEDED"
   }
 }
+}
+
