1a808e3cdd26029321b9ca8d3bef88643681b19a
pinkeshbhuwal-lgtm <pinkeshbhuwal@gmail.com>
2026-06-08 10:47:21 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,3 +1,25 @@
 resource "aws_s3_bucket" "glue_bucket" {
   bucket = "${var.project_name}-scripts-001"
 }
+
+resource "aws_s3_object" "job1_script" {
+
+  bucket = aws_s3_bucket.glue_bucket.id
+
+  key = "scripts/job1.py"
+
+  source = "glue-scripts/job1.py"
+
+  etag = filemd5("glue-scripts/job1.py")
+}
+
+resource "aws_s3_object" "job2_script" {
+
+  bucket = aws_s3_bucket.glue_bucket.id
+
+  key = "scripts/job2.py"
+
+  source = "glue-scripts/job2.py"
+
+  etag = filemd5("glue-scripts/job2.py")
+}
